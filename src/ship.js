d79ba20b83f27debc6fec0a901745329302b8bd7
thomasxcarney <thomas.carney26@gmail.com>
2023-11-27 14:11:02 -0500

    Create and test 'ship' factory function
    Created and tested factory function to create the ships in the game.

--- src/ship.js
+++ src/ship.js
@@ -2,7 +2,16 @@ const ship = function createShip(lengthOfShip){
     const length = lengthOfShip;
     const hits = 0;
     const sunk = 'not sunk';
-    return {length, hits, sunk};
+    const hit = function hit() {
+        this.hits+=1; 
+    };
+    const isSunk = function isSunk(){
+        if(this.hits >= this.length){
+            this.sunk = 'sunk';
+        };
+        return this.sunk;
+    };
+    return {length, hits, sunk, hit, isSunk};
 };
 
-export { ship }
+export default ship;
--- test.js
+++ test.js
@@ -5,3 +5,15 @@ test('ship factory creates object', () => {
     const myShip = ship(2);
     expect(myShip.length).toEqual(2);
 });
+
+test('ship hit function adds hits to object', () => {
+    const myShip = ship();
+    myShip.hit();
+    expect(myShip.hits).toEqual(1);
+});
+
+test('ship isSunk function calculates if ship is sunk based on length', () => {
+    const myShip = ship(1);
+    myShip.hit();
+    expect(myShip.isSunk()).toEqual("sunk");
+});
